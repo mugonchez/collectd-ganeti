50404f302e1fb98a39b53dd1446b9c4a6fcced3e
Moses Mugoya <mosesmugoya31@gmail.com>
2024-09-09 15:33:36 +0300

    Update kvm_io.py

--- kvm_io.py
+++ kvm_io.py
@@ -19,21 +19,25 @@ def read_io(data=None):
         M_read.host = "kvm_" + host
         M_read.plugin = "io_kvm"
         M_read.type_instance = "io_read"
+
         M_write = collectd.Values("counter")
         M_write.host = "kvm_" + host
         M_write.plugin = "io_kvm"
         M_write.type_instance = "io_write"
 
-        for line in open("/proc/%s/io" % pid, "r"):
-            if "read_bytes" in line:
-                M_read.values = [int(line.strip().split()[1])]
-            elif "write_bytes" in line:
-                M_write.values = [int(line.strip().split()[1])]
+        # Safely opening the file using a context manager
+        with open(f"/proc/{pid}/io", "r") as f:
+            for line in f:
+                if "read_bytes" in line:
+                    M_read.values = [int(line.strip().split()[1])]
+                elif "write_bytes" in line:
+                    M_write.values = [int(line.strip().split()[1])]
 
         M_read.dispatch()
         M_write.dispatch()
 
 
+# Register the configuration and read callbacks with collectd
 collectd.register_config(config_io)
 collectd.register_init(init_io)
 collectd.register_read(read_io)
