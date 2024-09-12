dbc55d77189f2d73478ac680a4f10e534fc2b3b8
Moses Mugoya <mosesmugoya31@gmail.com>
2024-09-12 22:18:17 +0300

    Update kvm_cpu_wait.py

--- kvm_cpu_wait.py
+++ kvm_cpu_wait.py
@@ -1,1 +1,11 @@
+# coding: utf-8
+import collectd
+from discover_vm import discover
 
+
+def config_cpu_wait(data=None):
+    collectd.debug("Configuration: " + repr(data))
+
+
+def init_cpu_wait(data=None):
+    collectd.debug("Initialization: " + repr(data))
