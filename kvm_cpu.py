3e49bf391921c8947d11d9cb71e60a21daa90f3f
Moses Mugoya <mosesmugoya31@gmail.com>
2024-09-12 22:20:24 +0300

    Update kvm_cpu.py

--- kvm_cpu.py
+++ kvm_cpu.py
@@ -28,31 +28,6 @@ def read_cpu(data=None):
         M.dispatch()
 
 
-def config_cpu_wait(data=None):
-    collectd.debug("Configuration: " + repr(data))
-
-
-def init_cpu_wait(data=None):
-    collectd.debug("Initialization: " + repr(data))
-
-
-def read_cpu_wait(data=None):
-    collectd.debug("Reading: " + repr(data))
-    for pid, host in discover().items():
-        # /var/lib/collectd/rrd/kvm_HOST/cpu_kvm/cpu-wait.rrd
-        M = collectd.Values("gauge")
-        M.host = "kvm_" + host
-        M.plugin = "cpu_kvm"
-        M.type_instance = "cpu_wait"
-        # Safely open the /proc/[pid]/stat file using a context manager
-        with open(f"/proc/{pid}/stat", 'r') as f:
-            fields = f.readline().split(' ')
-            user = fields[15]
-            system = fields[16]
-        M.values = [int(user) + int(system)]
-        M.dispatch()
-
-
 # Register the configurations and reading callbacks for collectd
 collectd.register_config(config_cpu)
 collectd.register_init(init_cpu)
